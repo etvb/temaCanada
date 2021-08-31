2644c25d80e6153b175e809c9c47331d599a2fad
etvb <edgardo.torres@hotmail.com>
2021-08-31 05:49:07 -0500

    creamos el loop basico y cremos el header

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+    get_header();
+    echo "desde PAGE.php";
+?>
+
+<?php while( have_posts() ) : the_post()?>
+<br>
+    <?php
+    the_title();
+
+    the_content();
+    ?>
+<?php endwhile;?>
