102a24187029f9d26f81dacd0a0fd793bc7e3225
etvb <edgardo.torres@hotmail.com>
2021-09-13 23:12:25 -0500

    fist steps for make a template

---
+++ page-blog.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Template Name: Guia toronto 
+ */
+
+get_header(); ?>
+
+<?php while( have_posts() ) : the_post()?>
+
+<?php if (has_post_thumbnail()) { ?>
+<div class="destacada">
+    <?php the_post_thumbnail('destacada'); ?>
+    <h2><?php the_title(); ?></h2>
+</div>
+<?php } else { ?>
+    <h2 class="noimagen"><?php the_title();?></h2>
+
+<?php }?>
+
+<div id="primary" class="primary">
+
+
+    <p>Desde la pagina del blog</p>
+</div>
+
+<?php endwhile;?>
+
+<?php get_sidebar();?>
+
+<?php get_footer();?>
